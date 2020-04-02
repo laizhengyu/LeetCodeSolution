e1303cad6941a999c2958a209f6fa9e7ddb2e705
laizhengyu <673470649@qq.com>
2020-04-02 21:46:28 +0800

    Create lc234.cpp

---
+++ Easy/lc234.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ //O(n) runtimes , O(1) spaces
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (head==NULL||head->next==NULL)
+            return true;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        //fast walk 2 steps ,slow walk 1 step. Then slow will stop in the middle
+        while(fast->next!=NULL&&fast->next->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        //reverse list beside the middle
+        slow->next=reverseList(slow->next);
+        slow=slow->next;
+        while(slow!=NULL)
+        {
+            if (head->val!=slow->val)
+                return false;
+            head=head->next;
+            slow=slow->next;
+        }
+        return true;
+        
+    }
+private:
+    ListNode* reverseList(ListNode* head)
+    {
+        
+        ListNode* pre=NULL;
+        ListNode* next=NULL;
+        while(head!=NULL)
+        {
+            next=head->next;
+            head->next=pre;
+            pre=head;
+            head=next;
+        }
+        return pre;
+    }
+};
